ae3e57f685be897d6a9cf1f4d3c1a5a748e55005
Dmytro Kolesnyk <ds.kolesnyk@gmail.com>
2021-12-09 22:09:42 +0200

    done (example 004)

--- 004_Console/Program.cs
+++ 004_Console/Program.cs
@@ -10,7 +10,47 @@ namespace _004_Console
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("***Basic Console I/O***");
+            Console.Beep();
+            GetUserData();
 
+            // Некоторые члены класса System.Console
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.ForegroundColor = ConsoleColor.Red;
+            //Console.BackgroundColor = ConsoleColor.Black;
+            Console.BufferHeight = 60;
+            Console.BufferWidth = 160;
+            Console.Title = "My own Title";
+            Console.WindowHeight = 20;
+            Console.WindowWidth = 100;
+            Console.WindowTop = 5; // начальное положение - отступ 5 строк 
+
+            Console.WriteLine("{0}, Number {0}, Number {0}", 9); // пример форматирования вывода с помощью метки-заполнителя
+            Console.WriteLine("{1}, {0}, {2}", 10, 20, 30); // пример форматирования вывода с помощью метки-заполнителя
+
+
+            Console.ReadLine();
+        }
+
+        static void GetUserData()
+        {
+            // Получение информации об имени и возрасте
+            Console.Write("Please enter your name: "); // запрос на ввод имени
+            string userName = Console.ReadLine();
+            Console.Write("Please enter your age: "); // запрос на ввод возраста
+            string userAge = Console.ReadLine();
+            // Изменение цвета изображения
+            ConsoleColor prevColor = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            // Отображение полученных сведений в окне консоли
+            Console.WriteLine("Hello {0}! You are {1} years old.", userName, userAge); // {0},{1} метки-заполнители
+            Console.WriteLine($"Hello {userName}! You are {userAge} years old." ); // метки-заполнители внутри стокового литерала
+
+
+
+
+            // Восстановление предыдущего цвета
+            Console.ForegroundColor = prevColor;
         }
     }
 }
---
+++ 004_Console_Formatting Numeric Data/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms; // namespace, подключенна ссылка на System.Windows.Forms
+
+namespace _004_Console_Formatting_Numeric_Data
+{
+    // Форматирование числовых данных
+    class Program
+    {
+        static void FormatNumericalData()
+        {
+            Console.WriteLine("***The value 99999 in various formats:");
+            Console.WriteLine("c format: {0:c}", 99999);   // форматирование денежных значений, передает символ локальной культуры
+            Console.WriteLine("d9 format: {0:d9}", 99999); // минимальное кол-во цифр для предоставления значения
+            Console.WriteLine("f3 format: {0:f3}", 99999); // форматирование числовых данных в формате с фиксированной точкой
+            Console.WriteLine("n format: {0:n}", 99999);   // базовое числовое форматирование (с запятыми)
+            Console.WriteLine("E format: {0:E}", 99999);   // экспоненциальное представление, верхний регистр
+            Console.WriteLine("e format: {0:e}", 99999);   // экспоненциальное представление, нижний регистр
+            Console.WriteLine("X format: {0:X}", 99999);   // шестнадцатиричный формат, верхний регистр
+            Console.WriteLine("x format: {0:x}", 99999);   // шестнадцатиричный формат, нижний регистр
+        }
+        static void DisplayMessage()
+        {
+            string userMessage = string.Format("100000 in hex is {0:x}", 100000);
+            MessageBox.Show(userMessage);
+        }
+        static void Main(string[] args)
+        {
+            FormatNumericalData();
+            DisplayMessage();
+        }
+    }
+}
