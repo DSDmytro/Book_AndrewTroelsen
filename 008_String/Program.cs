00ae6a473b56f794277f6f2353f1d00e0ab6b632
Dmytro Kolesnyk <ds.kolesnyk@gmail.com>
2021-12-21 22:02:55 +0200

    done (example 008, 009)

---
+++ 008_String/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _008_String
+{
+    class Program
+    {
+        static void BasicStringFunctionality()
+        {
+            // Базовые возможности типа String
+            Console.WriteLine("Basic String Functionality");
+            string firstName = "Freddy";
+            Console.WriteLine("Value of firstName: {0}", firstName);
+            Console.WriteLine("firstName has {0} characters", firstName.Length);
+            Console.WriteLine("firstName in uppercase: {0}", firstName.ToUpper());
+            Console.WriteLine("firstName in lowercase: {0}", firstName.ToLower());
+            Console.WriteLine("firstName contains the letter 'y'?: {0}", firstName.Contains('y'));
+            Console.WriteLine("firstName after replace: {0}", firstName.Replace("dy", ""));
+        }
+        static void StringConcatenation()
+        {
+            Console.WriteLine("String Concatenation");
+            string s1 = "Programming the ";
+            string s2 = "PsychoDrill (PTP)";
+            string s3 = s1 + s2;
+            string s4 = String.Concat(s1, s2); 
+            Console.WriteLine("s3 = s1 + s2: {0}", s3);
+            Console.WriteLine("s4 = String.Concat(s1, s2): {0}", s4);
+        }
+        static void Main(string[] args)
+        {
+            BasicStringFunctionality();
+            Console.WriteLine();
+            StringConcatenation();
+            Console.WriteLine();
+        }
+    }
+}
---
+++ 009_String_Escape characters, Verabtim string/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _009_String_Escape_characters__Verbatim_string
+{
+    class Program
+    {
+        static void EscapeChar()
+        {
+            Console.WriteLine("Escape Characters \a");
+            string strWithTabs = "Model \tColor \tSpeed \tPet name";
+            Console.WriteLine(strWithTabs);
+            Console.WriteLine("Every one loves \"Hello world\"");
+            Console.WriteLine("C:\\MyApp\\bin\\Debug");
+            Console.WriteLine("\n\n\n\n");
+            Console.WriteLine();
+        }
+        static void VerbatimString()
+        {
+            Console.WriteLine(@"C:\MyApp\bin\Debug");
+            Console.WriteLine(@"This is very 
+            very
+                very
+                    long string");
+            Console.WriteLine(@"Cerebus said ""Darrr! Pret-ty sun-sets""");
+        }
+        static void Main(string[] args)
+        {
+            EscapeChar();
+            Console.WriteLine();
+            VerbatimString();
+            Console.WriteLine();
+        }
+    }
+}
